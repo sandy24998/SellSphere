3cbd0ef7334dc87a4c674937f2c3091de8dd0b4c
Sandeep Pandit <54panditsandeep@gmail.com>
2025-10-05 15:29:06 +0000

    update -v2

--- back-end/src/config/db.js
+++ back-end/src/config/db.js
@@ -1,15 +1,25 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log(process.env.MONGO_URI)
-    console.log("✅ MongoDB Connected...");
+    const uri = process.env.MONGO_URI;
+    if (!uri) {
+      throw new Error("❌ Missing MONGO_URI");
+    }
+
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("✅ MongoDB connected successfully!");
   } catch (error) {
     console.error("❌ Database connection failed:", error.message);
-    process.exit(1); // Exit process with failure
+    process.exit(1);
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
