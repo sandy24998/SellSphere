26095f0319c6e721991ada032b3468f067d9d439
Sandeep Pandit <54panditsandeep@gmail.com>
2025-10-05 15:45:27 +0000

    update -v4

--- back-end/src/config/db.js
+++ back-end/src/config/db.js
@@ -3,12 +3,15 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log(process.env.MONGO_URI)
-    console.log("✅ MongoDB Connected...");
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error("❌ Database connection failed:", error.message);
-    process.exit(1); // Exit process with failure
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
   }
 };
 
