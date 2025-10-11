52459c01e9b254505493ee4b6cf74d3b65640e70
Sandeep Pandit <54panditsandeep@gmail.com>
2025-10-11 05:19:58 +0000

    updated server.js

--- back-end/server.js
+++ back-end/server.js
@@ -10,12 +10,13 @@ if (isNaN(PORT)) {
 }
 
 // ✅ Start server
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, '0.0.0.0', () => {
   console.log("========================================");
   console.log(`🌍 Environment: ${process.env.NODE_ENV || "development"}`);
   console.log(`🚀 Server is running on port: ${PORT}`);
   console.log("========================================");
 });
+
 
 // ✅ Graceful Shutdown
 const gracefulShutdown = (signal) => {
