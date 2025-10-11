875f641bab0688f52be2d29cc0e554c07fe6b4b2
Sandeep Pandit <54panditsandeep@gmail.com>
2025-10-11 05:14:50 +0000

    updated config and workflow file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,32 +1,23 @@
 module.exports = {
   apps: [
     {
-      name: "mern-backend",
-      script: "./back-end/server.js",
+      name: "backend",
+      script: "./server.js",
+      cwd: "./back-end",
       env: {
         NODE_ENV: "development",
         PORT: 5000,
-        MONGO_URI: "mongodb+srv://sandy54998:SNeCsU3iduibF0wb@cluster0.ypmdedl.mongodb.net/SellAndBuy?retryWrites=true&w=majority"
+        MONGO_URI: process.env.MONGO_URI
       },
-      watch: true,
-      ignore_watch: ["node_modules", "logs"],
-      error_file: "./logs/backend-err.log",
-      out_file: "./logs/backend-out.log"
     },
     {
-      name: "mern-frontend",
-      script: "npm",
-      args: "start",
+      name: "frontend",
+      script: "npx",
+      args: "serve -s build -l 3000",
       cwd: "./front-end",
       env: {
         NODE_ENV: "development",
-        PORT: 3000,
-        REACT_APP_API_URL: "http://localhost:5000/api"
       },
-      watch: true,
-      ignore_watch: ["node_modules", "build", "logs"],
-      error_file: "./logs/frontend-err.log",
-      out_file: "./logs/frontend-out.log"
-    }
-  ]
+    },
+  ],
 };
