005f3fdc59e6d65ccf2481ac849d2a36eefc2a24
Sandeep Pandit <54panditsandeep@gmail.com>
2025-10-11 06:34:44 +0000

    cors new changes added in app.js

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -8,11 +8,17 @@ connectDB();
 
 const app = express();
 
+// Enable CORS for frontend with preflight support
 app.use(cors({
   origin: 'http://localhost:3000', // frontend origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
+
+// Handle preflight requests globally
+app.options('*', cors());
+
 app.use(express.json()); // Correct json body parsing
 app.use(express.urlencoded({ extended: true })); // Correct urlencoded body parsing
 app.use('/api', router);
